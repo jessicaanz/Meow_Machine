2cde174174f76c7630d3c51c387ffed3f5225f0d
jessicaanz <jessicaanz1128@gmail.com>
2024-04-18 15:34:22 -0400

    p

--- dancing_robot_rpi.py
+++ dancing_robot_rpi.py
@@ -123,10 +123,13 @@ def leg_lifts():
 
 def twerk():
     move_2_servos_with_offsets(1, 4, -70, 70, straight_positions, positions, 1)
+    move_2_servos_with_offsets(2, 3, -30, 30, straight_positions, positions, 1)
     move_2_servos_with_offsets(5, 7, -10, 10, straight_positions, positions, 0.5)
     move_2_servos_with_offsets(5, 7, -40, 40, straight_positions, positions, 0.5)
     move_2_servos_with_offsets(5, 7, -10, 10, straight_positions, positions, 0.5)
     move_2_servos_with_offsets(5, 7, -40, 40, straight_positions, positions, 0.5)
+    move_2_servos_with_offsets(1, 4, -50, 50, straight_positions, positions, 1)
+    move_2_servos_with_offsets(2, 3, 40, 40, straight_positions, positions, 1)
 
 
 ##### MAIN METHOD #####
